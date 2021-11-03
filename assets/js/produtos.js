fdc62e3aea57cd110d13f9775672df42dc38516e
Matheus Alves <lucas24417@gmail.com>
2021-11-03 13:45:09 -0300

    as

--- assets/js/produtos.js
+++ assets/js/produtos.js
@@ -1,6 +1,6 @@
 IniciarBatatas = () => {
     var containerBatatas = document.getElementById('produtos');
-    var menu = 'https://teams.microsoft.com/_#/school/conversations/Not%C3%ADcias%20de%20Tecnologia?threadId=19:8871dea9c31c475c97a95a01fcfcdee9@thread.tacv2&ctx=channel';
+    var menu = 'https://mafia-da-batata.netlify.app/menu/';
     batataJson.map((val) => {
         containerBatatas.innerHTML += `
      <div class="col-lg-6 menu-item ${val.tipo}">
